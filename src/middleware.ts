7b7328439148b86e8b56ea454fee27de365dbe74
willframpton79-dot <willframpton79@gmail.com>
2026-06-05 16:26:32 +1000

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 export { default } from "next-auth/middleware";
 export const config = {
   matcher: [
-    "/((?!auth/signin|auth/signup|auth/error|api/auth|_next/static|_next/image|favicon.ico).*)",
+    "/((?!login|signup|api/auth|_next/static|_next/image|favicon.ico).*)",
   ],
 };
