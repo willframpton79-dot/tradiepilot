effc81ea9e0cfccaf1495856af0e7043f0aa2721
willframpton79-dot <willframpton79@gmail.com>
2026-06-05 15:37:00 +1000

    Update global.d.ts

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -2,3 +2,14 @@ declare module '*.css' {
   const content: { [className: string]: string };
   export default content;
 }
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      id: string;
+      email: string;
+      name?: string | null;
+      image?: string | null;
+    };
+  }
+}
